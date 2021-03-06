4350f7b921924d9d0f902c0f857f9cce3fde0172
Indranil-Sarmah <isharma055@gmail.com>
2021-03-06 23:28:18 +0530

    implemented onchange event on categories selection

---
+++ src/core/Checkbox.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+
+const Checkbox = ({ categories }) => {
+
+    const [checked, setCheked] = useState([]);
+
+    const handleToggle = c => () => { //higher order function  //this method will be fired when onchange is happening in the input
+
+        // return the first index or -1
+        const currentCategoryId = checked.indexOf(c); //category is already in the cecked state will find that by using indexOf(c) of the category array (Not found return -1 and found then index)
+        const newCheckedCategoryId = [...checked];
+        // if currently checked was not already in checked state > push
+        // else pull/take off
+        if (currentCategoryId === -1) {
+            newCheckedCategoryId.push(c);
+        } else {
+            newCheckedCategoryId.splice(currentCategoryId, 1); //we take off the category, 1 is denoting only one item
+        }
+        console.log(newCheckedCategoryId);
+        setCheked(newCheckedCategoryId);//update the state
+    };
+    
+    //c._id is category id
+    return categories.map((c, i) => (
+        <li key={i} className="list-unstyled">
+            <input onChange={handleToggle(c._id)}  value={checked.indexOf(c._id === -1)} type="checkbox" className="form-check-input" />
+            <label className="form-check-label">{c.name}</label>
+        </li>
+    ));
+};
+
+export default Checkbox;
--- src/core/Shop.js
+++ src/core/Shop.js
@@ -1,8 +1,29 @@
 import React, { useState, useEffect } from "react";
 import Layout from "./Layout";
 import Card from "./Card";
+import { getCategories } from "./apiCore";
+import Checkbox from "./Checkbox";
+
 
 const Shop = () => {
+
+    const [categories, setCategories] = useState([]); //state to hold the catefgories from backend
+    const [error, setError] = useState(false);
+
+    const init = () => {   //this will run automatically when components mount and load the categories using getCategories
+        getCategories().then(data => {
+            if (data.error) {
+                setError(data.error);
+            } else {
+                setCategories(data);
+            }
+        });
+    };
+
+    useEffect(() => {
+        init();
+    }, []); //when components mount
+
     return (
         <Layout
             title="Shop Page"
@@ -10,7 +31,12 @@ const Shop = () => {
             className="container-fluid"
         >
             <div className="row">
-                <div className="col-4">left sidebar</div>
+                <div className="col-md-4">
+                    <h4 className="text-light">Filter by categories</h4>
+                    <ul>
+                        <Checkbox categories={categories} />
+                    </ul>
+                </div>
 
                 <div className="col-8">right</div>
             </div>
