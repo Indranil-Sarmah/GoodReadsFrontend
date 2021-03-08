619ff8f0034b7ae75a741b8e19743fe4203f5c5c
Indranil-Sarmah <isharma055@gmail.com>
2021-03-08 23:39:11 +0530

    implenting filtering by rice range UI part completed

--- src/core/Shop.js
+++ src/core/Shop.js
@@ -3,9 +3,15 @@ import Layout from "./Layout";
 import Card from "./Card";
 import { getCategories } from "./apiCore";
 import Checkbox from "./Checkbox";
+import { prices } from "./fixedPrices";
+import RadioBox from "./RadioBox";
 
 
 const Shop = () => {
+
+    const [myFilters, setMyFilters] = useState({
+        filters: { category: [], price: [] }
+    });
 
     const [categories, setCategories] = useState([]); //state to hold the catefgories from backend
     const [error, setError] = useState(false);
@@ -25,7 +31,10 @@ const Shop = () => {
     }, []); //when components mount
 
     const handleFilters = (filters, filterBy) => { //filters and filtersBy
-        console.log("SHOP", filters, filterBy);
+        //console.log("SHOP", filters, filterBy);
+        const newFilters = { ...myFilters };
+        newFilters.filters[filterBy] = filters;
+        setMyFilters(newFilters);
     };
 
     return (
@@ -36,14 +45,24 @@ const Shop = () => {
         >
             <div className="row">
                 <div className="col-md-4">
-                    <h4 className="text-light">Filter by categories</h4>
+                    <h5 className="text-light">Filter by categories</h5>
                     <ul>
                         <Checkbox categories={categories} handleFilters={filters =>
                                 handleFilters(filters, "category")} />
                     </ul>
+                    
+                    <h5 className="text-light">Filter by price range</h5>
+                    <div >
+                        <RadioBox
+                            prices={prices}
+                            handleFilters={filters =>
+                                handleFilters(filters, "price")
+                            }
+                        />
+                    </div>
                 </div>
 
-                <div className="col-8">right</div>
+                <div className="col-8">{JSON.stringify(myFilters)}</div>
             </div>
         </Layout>
     );
---
+++ src/core/fixedPrices.js
@@ -0,0 +1,32 @@
+export const prices = [
+    {
+        _id: 0,
+        name: "Any",
+        array: []
+    },
+    {
+        _id: 1,
+        name: "100 Rs to 200 Rs",
+        array: [100, 200]
+    },
+    {
+        _id: 2,
+        name: "200 Rs to 500 Rs",
+        array: [201, 500]
+    },
+    {
+        _id: 3,
+        name: "500 Rs to 700 Rs",
+        array: [501, 700]
+    },
+    {
+        _id: 4,
+        name: "700 Rs to 1000 Rs",
+        array: [701, 1000]
+    },
+    {
+        _id: 5,
+        name: "More than 1000",
+        array: [1001, 1300]
+    }
+];
